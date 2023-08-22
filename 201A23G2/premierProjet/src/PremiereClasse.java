92d9fd932cf89289227c65aa4a0ef0bea6c734ec
Robert Aubé <robert.aube@cegeplimoilou.ca>
2023-08-22 05:04:30 -0400

    G2+

---
+++ 201A23G2/premierProjet/src/PremiereClasse.java
@@ -0,0 +1,10 @@
+public class PremiereClasse {
+    public static void main(String[] args) {
+        System.out.println("Bonjour le monde1!"); //afficher la chaîne de caractères "Bonjour le monde"
+        System.out.println("Bonjour le monde2!"); //afficher la chaîne de caractères "Bonjour le monde"
+        System.out.println("Bonjour le monde3!"); //afficher la chaîne de caractères "Bonjour le monde"
+        System.out.println("Bonjour le monde4!"); //afficher la chaîne de caractères "Bonjour le monde"
+        System.out.println("Bonjour le monde5!"); //afficher la chaîne de caractères "Bonjour le monde"
+    }
+}
+
