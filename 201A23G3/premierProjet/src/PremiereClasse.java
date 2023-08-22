93e5ddc5d181951463f91859a809181f5b956eef
Robert Aubé <robert.aube@cegeplimoilou.ca>
2023-08-22 17:37:21 -0400

    G3+

---
+++ 201A23G3/premierProjet/src/PremiereClasse.java
@@ -0,0 +1,8 @@
+public class PremiereClasse {
+    public static void main(String[] args) { // le main est la fonction où commence le programme
+        System.out.println("1Bonjour le monde!!!"); //afficher la chaine de caractères "Bonjour le monde!!!"
+        System.out.println("2Bonjour le monde!!!"); //afficher la chaine de caractères "Bonjour le monde!!!"
+        System.out.println("3Bonjour le monde!!!"); //afficher la chaine de caractères "Bonjour le monde!!!"
+        System.out.println("4Bonjour le monde!!!"); //afficher la chaine de caractères "Bonjour le monde!!!"
+    }
+}
