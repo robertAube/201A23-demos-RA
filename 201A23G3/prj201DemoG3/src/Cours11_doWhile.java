de262a815e680215f9237c3d6437c5418d961b1d
Robert Aubé <robert.aube@cegeplimoilou.ca>
2023-10-03 17:46:55 -0400

    3+

--- 201A23G3/prj201DemoG3/src/Cours11_doWhile.java
+++ 201A23G3/prj201DemoG3/src/Cours11_doWhile.java
@@ -2,15 +2,29 @@ import java.util.Scanner;
 
 public class Cours11_doWhile {
     public Cours11_doWhile() {
-        appuyerSurEspaceEtEnterPourContinuer_while();
+//        appuyerSurEspaceEtEnterPourContinuer_while();
+        appuyerSurEspaceEtEnterPourContinuer_dowhile();
     }
+
+
+    private void appuyerSurEspaceEtEnterPourContinuer_dowhile() {
+        String strSaisie;
+        //boucler sur cette question tant que l'usager n'a pas appuyée sur la barre d'espacement puis sur Enter.
+
+        do { //on fait une intération au minimum (on fait au moins une fois la boucle)
+            strSaisie = lireString("SVP appuyer sur la barre d'espacement puis sur Enter : ");
+            System.out.println("\"" + strSaisie + "\"");
+        } while (!strSaisie.equals(" ")); //condition après
+        System.out.println("Bravo d'avoir appuyé sur l'espace");
+    }
+
     private void appuyerSurEspaceEtEnterPourContinuer_while() {
         String strSaisie;
         //boucler sur cette question tant que l'usager n'a pas appuyée sur la barre d'espacement puis sur Enter.
 
         strSaisie = lireString("SVP appuyer sur la barre d'espacement puis sur Enter : ");
 
-        while (!strSaisie.equals(" ")) {
+        while (!strSaisie.equals(" ")) { //on est pas certain d'entrer dans la boucle
             System.out.println("\"" + strSaisie + "\"");
             strSaisie = lireString("SVP appuyer sur la barre d'espacement puis sur Enter : ");
         }
@@ -29,6 +43,7 @@ public class Cours11_doWhile {
 
         return reponse;
     }
+
     public static void main(String[] args) {
         new Cours11_doWhile();
     }
