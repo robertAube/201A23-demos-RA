296154959dc60389f8762cbb99e8f28ab2b45875
Robert Aubé <robert.aube@cegeplimoilou.ca>
2023-12-15 13:45:22 -0500

    G2 Pizza

--- 201A23G2/prj201DemoG2/src/partie2/cours25_ArrayList/ListePizza.java
+++ 201A23G2/prj201DemoG2/src/partie2/cours25_ArrayList/ListePizza.java
@@ -17,12 +17,12 @@ public class ListePizza {
         listePizza.remove(i);
     }
     public void enleverPizza(Pizza p) {
-        listePizza.remove(p);
+        listePizza.remove(p);  // possible grâce à l'utilisation du equals() de Pizza
     }
 
     public void avancerPizza() {
         for (int i = 0; i < listePizza.size(); i++) {
-            listePizza.get(i).avancerX();
+            listePizza.get(i).avancerX(); // on a pas à faire de cast sur "listePizza.get(i)" parce qu'on a une ArrayList de <Pizza>
         }
     }
 
